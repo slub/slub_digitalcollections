2a80ceda3fe21b0640de89d1e7cba62b7134bc90
Alexander Bigga <alexander.bigga@slub-dresden.de>
2020-12-26 20:51:11 +0100

    Reorganize TsConfig and add newContentElement wizard

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,20 +1,11 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') || die();
 
-if (TYPO3_MODE === 'BE') {
-
+call_user_func(function()
+{
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile (
         'slub_digitalcollections',
-        'Configuration/TSconfig/pageConfig.ts',
-        'SLUB Digital Collections: Page TS'
+        'Configuration/TsConfig/All.tsconfig',
+        'SLUB Digital Collections: Page TS incl. Backend Layouts'
     );
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-        'slub_digitalcollections',
-        'Configuration/TSconfig/BackendLayouts.ts',
-        'SLUB Digital Collections: Backend Layouts'
-    );
-
-}
+});
