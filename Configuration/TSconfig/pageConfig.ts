76bf3cf8194b31edb74637aad5b1ded149d4a6e9
Alexander Bigga <alexander.bigga@slub-dresden.de>
2020-12-26 20:51:11 +0100

    Reorganize TsConfig and add newContentElement wizard

--- Configuration/TSconfig/pageConfig.ts
+++
@@ -1,5 +0,0 @@
-###################################################
-# Page TS settings
-###################################################
-
-mod.web_list.allowedNewTables = tx_dlf_metadata,tx_dlf_structures,tx_dlf_libraries,tx_dlf_collections
