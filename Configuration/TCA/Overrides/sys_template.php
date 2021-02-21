8d78fb43c469fc122c62a8243b4b9a2719865c86
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-02-21 22:59:20 +0100

    Add optional sitepackage configuration to run without extra sitepackage

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -9,3 +9,10 @@ if (!defined('TYPO3_MODE')) {
     'Configuration/TypoScript',
     'SLUB Digital Collections'
 );
+
+// Add optional SitePackage Typoscript
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'slub_digitalcollections',
+    'Configuration/TypoScript/SitePackage',
+    'Optional Sitepackage for pure usage of SLUB Digital Collections.'
+);
