232b99910060fbc5ad8840106c9dd110bda17e47
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-03-19 18:06:48 +0100

    No need to use switchableControllerAction here

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,7 +1,5 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
 // Add default Typoscript
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
