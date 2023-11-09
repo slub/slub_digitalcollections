905fe09fc8117160ba1ed7563a02b4ee3a76a028
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2023-11-09 14:13:41 +0100

    Fix return for `getParametersSafely`

--- Classes/Controller/SingleCollectionController.php
+++ Classes/Controller/SingleCollectionController.php
@@ -129,8 +129,6 @@ class SingleCollectionController extends \TYPO3\CMS\Extbase\Mvc\Controller\Actio
 
         // set default sorting
         if (!isset($searchParams['orderBy'])) {
-            // TODO: Cannot assign offset 'orderBy' to string.
-            // @phpstan-ignore-next-line
             $searchParams['orderBy'] = 'title_usi';
             $searchParams['order'] = 'asc';
         }
@@ -167,7 +165,7 @@ class SingleCollectionController extends \TYPO3\CMS\Extbase\Mvc\Controller\Actio
      *
      * @param string $parameterName
      *
-     * @return null|string
+     * @return null|string|array
      */
     protected function getParametersSafely($parameterName)
     {
