351c3d4b52db78ccd860d501f0244aecc540e098
Florian Schölzel <fschoelzel.kontakt@gmail.com>
2024-03-25 17:32:23 +0100

    add typo3 v11.5 and dropped v9 Support in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '3.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '9.5.31-10.4.99',
+			'typo3' => '10.4.99-11.5.99',
 		),
 	),
 );
