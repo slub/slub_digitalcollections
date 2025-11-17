abf210d52b2166a0fe8efcdbfdb9d177323f764d
Markus Weigelt <markus.weigelt@slub-dresden.de>
2025-11-17 13:30:59 +0100

    Reset file

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,10 +18,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'version' => '4.1.0',
-	'constraints' => array(
+    'constraints' => array(
         'depends' => [
-            'php' => '8.2.0-8.4.99',
-            'typo3' => '12.4.0-13.4.99'
+            'php' => '8.1.0-8.4.99',
+            'typo3' => '11.5.0-13.4.99'
         ],
-	),
+    ),
 );
