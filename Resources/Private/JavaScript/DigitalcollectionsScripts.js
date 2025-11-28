c6498b6011a5ccb6a92d90c87d35568a00510925
Florian Schölzel <fschoelzel.kontakt@gmail.com>
2025-11-28 20:55:05 +0100

    Update emptyMessage for multivolume_work
    - because `tx-dlf-map` is set in setup.typoscript for `elementId`

--- Resources/Private/JavaScript/DigitalcollectionsScripts.js
+++ Resources/Private/JavaScript/DigitalcollectionsScripts.js
@@ -204,11 +204,23 @@ $(function () {
         }
     }
 
-    // Add a error message if no map element in document viewer given
-    if (!$('.tx-dlf-pageview').children()[0]) {
-        emptyMessage = ($('html[lang^="de"]')[0]) ? 'Kein Band ausgew&auml;hlt. Klicken Sie hier um zum ersten Band dieses Werks zu gelangen.' : 'No volume selected. Click to jump to the first available volume.';
-        $('.tx-dlf-pageview').append('<div class="tx-dlf-empty"><a class="tx-dlf-emptyToFirstVol" href="' + $('.tx-dlf-toc ul li ul li:first-child a').attr('href') + '"><span class="error-arrow">&larr;</span>' + emptyMessage + '</a></div>');
+    // Add an error message if no map element in document viewer is given
+    function checkForChild() {
+        const hasChild = $('.tx-dlf-pageview').children('.tx-dlf-map').children()[0];
+        
+        if (!hasChild) {
+            const emptyMessage = ($('html[lang^="de"]')[0])
+                ? 'Kein Band ausgew&auml;hlt. Klicken Sie hier um zum ersten Band dieses Werks zu gelangen.'
+                : 'No volume selected. Click to jump to the first available volume.';
+
+            $('.tx-dlf-pageview').children('.tx-dlf-map').remove();
+            $('.tx-dlf-pageview').append('<div class="tx-dlf-empty"><a class="tx-dlf-emptyToFirstVol" href="' + $('.tx-dlf-toc ul li ul li:first-child a').attr('href') + '"><span class="error-arrow">&larr;</span>' + emptyMessage + '</a></div>');
+        }
     }
+
+    // Wait for 500ms to give OpenLayers time to populate the .tx-dlf-map Element
+    setTimeout(checkForChild, 500);
+
 
     // Add class to  collection related DD elements in metadata lists
     $('dl.tx-dlf-metadata-titledata').find('dt:contains(mmlung), dt:contains(llection)').nextUntil('dt', 'dd').addClass('tx-dlf-metadata-collection');
