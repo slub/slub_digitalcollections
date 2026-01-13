cd89096d8d3c279c1e67053d03d5955724aa5c8d
Carsten Radeck <carsten.radeck@slub-dresden.de>
2026-01-13 10:46:54 +0100

    add initial find configuration

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -14,3 +14,10 @@ defined('TYPO3') or die('Access denied.');
     'Configuration/TypoScript/SitePackage',
     'Optional Sitepackage for pure usage of SLUB Digital Collections.'
 );
+
+// Add optional find configuration Typoscript
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'slub_digitalcollections',
+    'Configuration/TypoScript/Plugin/Find',
+    'Optional find configuration for SLUB Digital Collections.'
+);
